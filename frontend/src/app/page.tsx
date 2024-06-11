ef977769edd7f4ec0991be057a4a1e52869553da
Sarvesh <sarveshjaiofficial@gmail.com>
2024-06-12 03:10:31 +0530

    Bug Fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/dashboard");
-  });
+    router.push("/signin");
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
